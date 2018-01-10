08c533ba50655dfe7977c7a5b890be422f757e44
Barkha <barkhajain54@gmail.com>
2018-01-10 22:49:33 +0530

    A simple Notification component using AngularJS

--- app/main.js
+++ app/main.js
@@ -13,17 +13,65 @@ angular.module('app', ['amqp-091', 'smart-table', 'ui.bootstrap'])
             routingKey: 'person'
         };
         $scope.person = {
-            name: 'anonymous', age: 25
+            message: 'Barkha', category: ''
         };
-        $scope.rowCollection = [
-            {name: 'Achmad Nasirudin Sandi', age: 26}
+        $scope.assigned_tasks = 45;
+        $scope.reminders = 4;
+        $scope.notifications = 14;
+        $scope.toggle = true;
+        $scope.toggleCustom = function() {
+            $scope.toggle = $scope.toggle === false? true: false;
+            $scope.count = 0;
+        };
+        var date = new Date();
+        $scope.count = 0;
+        var weekday = new Array(7);
+        weekday[0] =  "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+        var monthNames = [
+            "Jan", "Feb", "Mar",
+            "Apr", "May", "June", "July",
+            "Aug", "Sept", "Oct",
+            "Nov", "Dec"         
         ];
+        var day = date.getDate();
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+        var dayOfWeek = date.getDay();
+        $scope.dateForFormat = Date.now();
+        $scope.dateToDisplay = weekday[dayOfWeek]+' '+ monthNames[monthIndex]+' '+day+', '+year;
+        console.log($scope.dateToDisplay);
+        $scope.rowCollection = [
+            {message: 'Oliver has assigned you an amazing interview', category: 'Assigned tasks', date: "Yesterday"},
+            {message: 'Barkha  is great and she Rocks!', category: 'Notifications', date: "Yesterday"}
+        ];
+        $scope.rowCollection.forEach(function(entry) {
+            
+            console.log(entry);
+        });
         $scope.displayedCollection = [].concat($scope.rowCollection);
 
         $scope.greeting = {};
         $scope.send = function () {
             $scope.$emit('person.send');
         };
+
+        $scope.removeRow = function(index) {
+            if($scope.displayedCollection[index].category == 'Assigned tasks') {
+                $scope.assigned_tasks--;
+            } else if($scope.displayedCollection[index].category == 'Reminders') {
+                $scope.reminders--;
+            } else {
+                $scope.notifications--;
+            }
+            $scope.displayedCollection.splice(index, 1);
+
+        }
         amqp.connect(amqpConfig).then(function () {
 
             var consumeChannel = amqp.client.openChannel(function (channel) {
@@ -44,7 +92,7 @@ angular.module('app', ['amqp-091', 'smart-table', 'ui.bootstrap'])
                 });
                 consumeChannel.addEventListener('message', function (event) {
                     var body = amqpUtil.getBodyAsString(event);
-                    $scope.rowCollection.push(angular.fromJson(body));
+                    $scope.rowCollection.unshift(angular.fromJson(body));
                     $scope.$applyAsync();
                     setTimeout(function () {
                         event.target.ackBasic({
@@ -71,11 +119,43 @@ angular.module('app', ['amqp-091', 'smart-table', 'ui.bootstrap'])
                 });
                 $scope.$on('person.send', function () {
                     console.log('Sending ' + angular.toJson($scope.person));
+                    $scope.count++;
+                    if($scope.person.category == 'Notifications') {
+                        $scope.notifications++;
+                        console.log($scope.notifications);
+                    } else if($scope.person.category == 'Reminders') {
+                        $scope.reminders++;
+                        console.log($scope.reminders);
+                    } else {
+                        $scope.assigned_tasks++;
+                        console.log($scope.assigned_tasks);
+                    }
                     var body = amqpUtil.createMessageBody($scope.person);
                     var props = new Kaazing.AMQP.AmqpProperties();
                     publishChannel.publishBasic({body: body, properties: props, exchange: amqpConfig.exchange, routingKey: amqpConfig.routingKey});
                 });
             });
+            $scope.prettyDate = function (time){
+                console.log(time);
+                    var diff = (((new Date()).getTime() - time) / 1000),
+                    day_diff = Math.floor(diff / 86400);
+                        
+                if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
+                    return;
+                        
+                var res = day_diff == 0 && (
+                        diff < 60 && "Just Now" ||
+                        diff < 120 && "1 minute ago" ||
+                        diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+                        diff < 7200 && "1 hour ago" ||
+                        diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+                    day_diff == 1 && "Yesterday" ||
+                    day_diff < 7 && day_diff + " days ago" ||
+                    day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+                    console.log(res);
+                    $scope.person['date'] = res;
+
+            }
             var unregister = $scope.$on('$routeChangeStart', function () {
                 console.log('Route changed');
                 consumeChannel.closeChannel({});
